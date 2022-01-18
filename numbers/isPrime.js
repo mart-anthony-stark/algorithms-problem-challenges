4082084f353a5275c7ca70a6adbeb181e76e0720
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-18 18:59:38 +0800

    Added isPrime function

---
+++ numbers/isPrime.js
@@ -0,0 +1,29 @@
+const isPrime = (n) => {
+  if (n <= 1) return false;
+
+  for (let i = 0; i < n; i++) {
+    if (n % 0 == 0) return false;
+  }
+
+  return true;
+};
+
+const isPrime2 = (n) => {
+  if (n <= 1) return false;
+  if (n <= 3) return true;
+
+  if (n % 2 == 0 || n % 3 == 0) return false;
+
+  for (let i = 5; i * i <= n; i = i + 6) {
+    if (n % i == 0 || n % (i + 2) == 0) return false;
+  }
+
+  return true;
+};
+console.log(isPrime2(1));
+console.log(isPrime2(2));
+console.log(isPrime2(3));
+console.log(isPrime2(4));
+console.log(isPrime2(5));
+console.log(isPrime2(6));
+console.log(isPrime2(7));
