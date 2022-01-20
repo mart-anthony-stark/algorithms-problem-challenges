878bdde5c7e0e7c3a9a03bf5eca2329532eb529b
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-20 21:51:26 +0800

    Added hackerrank solution

--- challenges/fib.js
+++ challenges/fib.js
@@ -3,7 +3,15 @@ const fib = (n) => {
 
   return fib(n - 1) + fib(n - 2);
 };
-console.log(fib(6));
-console.log(fib(7));
-console.log(fib(8));
+// console.log(fib(6));
+// console.log(fib(7));
+// console.log(fib(8));
+
+let sum = 0;
+const foo = (n) => {
+  if (n <= 1) return sum;
+  return foo(n - 1);
+};
+console.log(foo(5));
+
 module.exports = fib;
