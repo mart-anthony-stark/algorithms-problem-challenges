244f9097292a2210a6acc149f2834893e8eaec34
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-17 21:41:19 +0800

    add: fibonacci method optimization

--- challenges/fib.js
+++ challenges/fib.js
@@ -1,9 +1,18 @@
-const fib = (n) => {
+// const fib = (n) => {
+//   if (n <= 2) return 1;
+
+//   return fib(n - 1) + fib(n - 2);
+// };
+const fib = (n, cache = {}) => {
+  if (cache[n]) return cache[n];
   if (n <= 2) return 1;
 
-  return fib(n - 1) + fib(n - 2);
+  
+  cache[n] = fib(n-1, cache) + fib(n-2, cache);
+  return cache[n]
 };
-// console.log(fib(6));
+
+console.log(fib(6));
 // console.log(fib(7));
 // console.log(fib(8));
 
