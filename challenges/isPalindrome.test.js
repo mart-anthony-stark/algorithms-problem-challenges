dd0b8f70b182b8aac923b50679f73563fa38e8c3
mart-anthony-stark <martsalazar0@gmail.com>
2022-09-18 21:40:23 +0800

    moved to js directory

--- challenges/isPalindrome.test.js
+++
@@ -1,8 +0,0 @@
-const isPalindrome = require("./isPalindrome");
-
-test("Properly checks palindrome strings", () => {
-  expect(isPalindrome("maam")).toBe(true);
-  expect(isPalindrome("2002")).toBe(true);
-  expect(isPalindrome("mart")).toBe(false);
-  expect(isPalindrome("ABCDCBA")).toBe(true);
-});
