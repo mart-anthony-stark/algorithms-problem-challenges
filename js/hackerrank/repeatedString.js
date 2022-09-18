b0af29b1a0a47874191004662821000c47b8295c
mart-anthony-stark <martsalazar0@gmail.com>
2022-09-18 21:42:05 +0800

    moved to js directory

---
+++ js/hackerrank/repeatedString.js
@@ -0,0 +1,23 @@
+const repeatedString = (s, n) => {
+  const min = 1;
+  const maxs = 100;
+  const maxn = 1000000000000;
+  let as = s.split("").filter((i) => i === "a").length;
+  if (
+    typeof s === "string" &&
+    s.length >= min &&
+    s.length <= maxs &&
+    n === parseInt(n, 0) &&
+    n >= min &&
+    n <= maxn
+  )
+    as =
+      parseInt(n / s.length, 0) * as +
+      s
+        .slice(0, n % s.length)
+        .split("")
+        .filter((i) => i === "a").length;
+
+  return as;
+}
+module.exports = repeatedString
