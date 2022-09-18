b9e254009efe19173017dbe36dee16e7f80b0f8d
mart-anthony-stark <martsalazar0@gmail.com>
2022-09-18 21:40:59 +0800

    moved to js directory

--- hackerrank/palindromeIndex.test.js
+++
@@ -1,7 +0,0 @@
-const palindromeIndex = require("./palindromeIndex");
-
-test("Returns the index to be removed to make the string palindrome", () => {
-  expect(palindromeIndex("aaab")).toBe(3);
-  expect(palindromeIndex("baa")).toBe(0);
-  expect(palindromeIndex("aaa")).toBe(-1);
-});
