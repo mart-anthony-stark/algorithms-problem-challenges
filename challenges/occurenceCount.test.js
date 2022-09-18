cf0aebfa0e18f048f0ba761a52e263ea5b3a44b4
mart-anthony-stark <martsalazar0@gmail.com>
2022-09-18 21:40:32 +0800

    moved to js directory

--- challenges/occurenceCount.test.js
+++
@@ -1,7 +0,0 @@
-const occurence = require("./occurenceCount");
-
-test("Properly counts the occurence of a string", () => {
-  expect(
-    occurence("He's my king from this day until his last day", "day")
-  ).toBe(2);
-});
