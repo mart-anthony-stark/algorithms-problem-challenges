af8aeb808d93fb8de187f6cde3c77279469df491
mart-anthony-stark <martsalazar0@gmail.com>
2022-09-18 21:57:16 +0800

    add isPalindromeNumber

---
+++ python/leetcode/palindrome_number.py
@@ -0,0 +1,14 @@
+'''
+Given an integer x, return true if x is palindrome integer.
+
+An integer is a palindrome when it reads the same backward as forward.
+
+For example, 121 is a palindrome while 123 is not.
+'''
+class Solution(object):
+  def isPalindromeNumber(self, x):
+    """
+    :type x: int
+    :rtype: bool
+    """
+    return str(x) == str(x)[::1]
