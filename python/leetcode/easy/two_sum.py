2a737ed2dbaa5bd9fcfc29a632f367611ee051d0
mart-anthony-stark <martsalazar0@gmail.com>
2022-09-19 21:51:39 +0800

    moved dir

---
+++ python/leetcode/easy/two_sum.py
@@ -0,0 +1,21 @@
+'''
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+'''
+
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        values = {}
+        for index, value in enumerate(nums):
+            difference = target - value
+            if difference in values:
+                return [values[difference], index]
+            else:
+                values[value] = index
