cdbd6db00865ffea389f102f3c8905db42b89f98
Mart Anthony Salazar <mart.salazar.pixel8@gmail.com>
2023-01-08 17:35:09 +0800

    Add gcd lcm

--- python/competitive/num_t.py
+++ python/competitive/num_t.py
@@ -1,7 +1,11 @@
-n = 10
-def sum1():
+def sum1(n):
     # O(1)
     return (n) * (n+1) // 2
+
+def sum2(n1, n2):
+    # O(1)
+    n = n2-n1
+    return (n//2) * (n1+n2)
 
 def get2():
     # O(n)
@@ -14,4 +18,20 @@ def get2():
 
 # O(n) > O(1)
 # O(1) is efficient
-print(sum1())
+print("Sum of consecutive numbers to 10: {}".format(sum1(10)))
+print("Sum of consecutive numbers to 10: {}".format(sum2(0, 10)))
+
+
+# GREATEST COMMON DENOMINATOR / LOWEST COMMON FACTOR
+# Euclid algo
+# T.C = O(log(min(a/b)))
+# produc = lcm * gcd
+def gcd(a,b):
+    if a == 0:
+        return b
+    return(b%a, a)
+
+def lcm(a,b):
+    prod = a*b
+    hcf = gcd(a,b)
+    return prod // hcf
