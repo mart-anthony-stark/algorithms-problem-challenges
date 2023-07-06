31daed523d3ab1d926745cc60849087a5d4dcee2
mart-anthony-stark <martsalazar0@gmail.com>
2023-07-06 22:30:56 +0800

    Add: questions array

---
+++ js/interview-questions/questions.js
@@ -0,0 +1,84 @@
+const questions = [
+  {
+    _id: 1,
+    question: "What is a data structure?",
+    answers: [
+      "A data structure is a storage format that defines the way data is stored, organized, and manipulated.",
+      "Some popular data structures are Arrays, Trees, and Graphs",
+    ],
+  },
+  {
+    _id: 2,
+    question: "What is an Array?",
+    answers: [
+      "An array is commonly referred to as a collection of items stored at contiguous memory locations.",
+      "Items stored are of the same type.",
+      "It organizes data so that a related set of values can be easily sorted or searched.",
+    ],
+  },
+  {
+    _id: 3,
+    question: "What is a Linked List?",
+    answers: [
+      "Like an array, a linked list refers to a linear data structure in which the elements are not necessarily stored in a contiguous manner.",
+      "It is basically a sequence of nodes, each node points towards the next node forming a chain-like structure.",
+    ],
+  },
+  {
+    _id: 4,
+    question: "What is LIFO?",
+    answers: [
+      "LIFO is an abbreviation for Last In First Out",
+      "It is a way of accessing, storing and retrieving data.",
+      "It extracts the data that was stored last first.",
+    ],
+  },
+  {
+    _id: 5,
+    question: "What is a Stack?",
+    answers: [
+      "A stack refers to a linear data structure performing operations in a LIFO (Last In First Out) order.",
+      "In a stack, elements can only be accessed, starting from the topmost to the bottom element.",
+    ],
+  },
+  {
+    _id: 6,
+    question: "What is FIFO?",
+    answers: [
+      "FIFO stands for First In First Out.",
+      "It is a way of accessing, storing and retrieving data.",
+      "The data that was stored first is extracted first.",
+    ],
+  },
+  {
+    _id: 7,
+    question: "What is a Queue?",
+    answers: [
+      "A queue refers to a linear data structure that performs operations in a FIFO order.",
+      "In a queue, the least recently added elements are removed first as opposed to a stack. ",
+    ],
+  },
+  {
+    _id: 8,
+    question: "What are Binary Trees?",
+    answers: [
+      "A binary tree is an extension of the linked list structure where each node has at most two children.",
+      "A binary tree has two nodes at all times, a left node and a right node.",
+    ],
+  },
+  {
+    _id: 9,
+    question: "What is Recursion?",
+    answers: [
+      "Recursion refers to a function calling itself based on a terminating condition.",
+      "It uses LIFO and therefore makes use of the stack data structure.",
+    ],
+  },
+  {
+    _id: 10,
+    question: "What is the OOPs concept?",
+    answers: [
+      "OOP stands for Object-Oriented Programming System, a paradigm that provides concepts such as objects, classes, and inheritance.",
+    ],
+  },
+];
